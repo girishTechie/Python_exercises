fbb04f76f6240c95e86128414d8860ecd41af358
girishTechie <mgirish540@gmail.com>
2025-10-16 08:44:59 +0530

    reusing existing variables

--- master.py
+++ master.py
@@ -1,12 +1,12 @@
 total = 10000
-#effort = 4
-effort = int(input("what is your daily efffort? "))
+#effort = 3
+effort = int(input("what is your daily effort? "))
 
 days = total // effort
 rem_hrs = total % effort
 months = days // 30
-rem_days = days % 30 
+days = days % 30 
 years = months // 12 
-rem_months = months % 12
+months = months % 12
 
-print(years, "YRS", rem_months, "MNTHS", rem_days, "DAYS", rem_hrs, "HRS")
+print(years, "YRS", months, "MNTHS", days, "DAYS", rem_hrs, "HRS")
