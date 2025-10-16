f26ef779648425816952c6ab7352768db3eaf7c6
girishTechie <mgirish540@gmail.com>
2025-10-16 08:50:59 +0530

    using divide function

--- master.py
+++ master.py
@@ -1,12 +1,21 @@
-total = 10000
-#effort = 3
-effort = int(input("what is your daily effort? "))
+def divide(num, den):
+    quot = num // den 
+    rem = num % den
+    return quot, rem 
 
-days = total // effort
-rem_hrs = total % effort
-months = days // 30
-days = days % 30 
-years = months // 12 
-months = months % 12
+total = 10000
+effort = 3
+
+#days = total // effort
+#rem_hrs = total % effort
+days, rem_hrs = divide(total, effort)
+
+#months = days // 30
+#days = days % 30 
+months, days = divide(days, 30) 
+
+#years = months // 12 
+#months = months % 12
+years, months = divide(months, 12)
 
 print(years, "YRS", months, "MNTHS", days, "DAYS", rem_hrs, "HRS")
