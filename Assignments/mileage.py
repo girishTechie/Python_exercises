10fd121365a4316bcccd82f88229b3914310455d
girishTechie <mgirish540@gmail.com>
2025-10-16 21:15:35 +0530

    calculate its mileage

---
+++ Assignments/mileage.py
@@ -0,0 +1,7 @@
+total_travel = 100 
+#starting_point = 5
+#ending_point =105
+#total_travel = ending_point - starting_point
+utilized_fuel = int(input("enter how much fuel consumed? "))
+milage = total_travel / utilized_fuel
+print(milage)
